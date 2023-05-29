b69c46c11167ec6d3f879bdd1d81fc1cae4fc71f
Hoaveth <hoaveth.gaming@gmail.com>
2023-05-29 14:57:03 +0800

    Added responsive layouts with chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-import { Flex, Box } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Content from "./components/Content";
 
 function App() {
   return (
-    <div className="App">
-      //Search Ciomponent //SideBar Menu
-      <Flex color="white">
-        <Box w="100px" h="100px"></Box>
-      </Flex>
-      //Main Content
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem pl="2" bg="orange.300" area={"nav"}>
+        <Header />
+      </GridItem>
+      <Show above="lg">
+        <GridItem pl="2" bg="pink.300" area={"aside"}>
+          <Sidebar />
+        </GridItem>
+      </Show>
+      <GridItem pl="2" bg="green.300" area={"main"}>
+        <Content />
+      </GridItem>
+    </Grid>
   );
 }
 
