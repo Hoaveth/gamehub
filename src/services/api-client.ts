d3a8bd7d02a254206be95cd595bf480b36efd571
Hoaveth <hoaveth.gaming@gmail.com>
2023-05-29 22:16:50 +0800

    Removed important key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "f80e80138ecd410ba62fc207be40bb60",
+    key: import.meta.env.VITE_RAWG_API_KEY,
   },
 });
