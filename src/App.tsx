442f8b5cd83b3e59807fe163542b06eb860b32dc
Hoaveth <hoaveth.gaming@gmail.com>
2023-05-29 14:38:56 +0800

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { Flex, Box } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+      //Search Ciomponent //SideBar Menu
+      <Flex color="white">
+        <Box w="100px" h="100px"></Box>
+      </Flex>
+      //Main Content
+    </div>
+  );
+}
+
+export default App;
